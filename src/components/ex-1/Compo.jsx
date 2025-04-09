e29a6c75aef5235823b028ac52f1d91d22d1e741
Abdullah Al Shahed <abdullahalshahedkhan@gmail.com>
2025-04-09 14:09:43 +0600

    learned props drealing

--- src/components/ex-1/Compo.jsx
+++ src/components/ex-1/Compo.jsx
@@ -1,10 +1,12 @@
-const Compo = (props) => {
+import { Compo2 } from './Compo2';
+const Compo = ({name, email, age}) => {
     // console.log(props);
     // const name = props.name;
-    const { name, email, age } = props;
+    // const { name, email, age } = props;
     return (
         <div>
             <h1>Hello from component - {name}, {email}, {age}</h1>
+            <Compo2 name={name} email={email} age={age}/>
         </div>
     )
 }
---
+++ src/components/ex-1/Compo2.jsx
@@ -0,0 +1,9 @@
+
+export const Compo2 = ({name, email, age}) => {
+    console.log(email);
+  return (
+    <div>
+        <h1 className="text-bold text-2xl">Hello from Compo2</h1>
+    </div>
+  )
+}
