ad9c0873b76dbc97f5107aaeb29c6ad64ac703e1
Abdullah Al Shahed <abdullahalshahedkhan@gmail.com>
2025-04-10 10:39:45 +0600

    learne use effect full

--- src/components/ex-3/Todo.jsx
+++ src/components/ex-3/Todo.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Todo = () => {
   const [input, setInput] = useState("");
+  const [text, setText] = useState(""); // Declare the state for text
   const [todos, setTodos] = useState([]); // Declare the state for todos
 
 
@@ -13,20 +14,23 @@ const Todo = () => {
 
   useEffect(() => {
     fetchTodo();
+    console.log("use effect called")
   }, []);
 
-  console.log("todo component is rendered");
+  console.log("component rendered");
 
   return (
     <div>
       <h1 className="font-bold text-2xl">Todo</h1>
       {
         todos.map((todo) =>(
-            <div key={todo} className="bg-gray-100 rounded p-2 m-2">
+            <div key={todo.id} className="bg-gray-100 rounded p-2 m-2">
                 <h1>{todo.title}</h1>
             </div>
         ))
       }
+      <input onChange={(e) => setInput(e.target.value)} type="text" placeholder="search..." className="border border-gray-300 rounded p-2 m-2" />
+      <input onChange={(e) => setText(e.target.value)} type="text" placeholder="text..." className="border border-gray-300 rounded p-2 m-2" />
     </div>
   );
 };
